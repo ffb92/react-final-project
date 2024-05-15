576c31bbb4840b81292b15fcca01ec99ec8e699b
ffb92 <finalflash.benni@gmail.com>
2024-05-15 12:29:22 +0200

    Automated commit

--- src/pages/PicturesScreen.jsx
+++ src/pages/PicturesScreen.jsx
@@ -45,8 +45,8 @@ const PicturesScreen = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-red-400/50 via-rose-500/75 to-red-600/75">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 w-5/6">
+    <div className="flex justify-center items-center h-full sm:h-screen bg-gradient-to-r from-red-400/50 via-rose-500/75 to-red-600/75">
+      <div className=" py-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 w-5/6">
         {picturesInfo.map((info, index) => (
           <Card key={index}>
             <CardHeader className="flex justify-center gap-3">
